7f7011c3f4d1870195a8a171a4dcf805e6d39bbc
Graham Mickle <graham@adboomgrp.com>
2014-12-03 19:49:44 -0800

    initial commit

---
+++ git-standup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function yesterworkday()
+{
+    if [[ "1" == "$(date +%u)" ]]
+    then
+        echo "last friday"
+    else
+        echo "yesterday"
+    fi
+}
+
+git log --since="$(yesterworkday)" --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --author="graham"
